207d796e0857ce01c315ab53e16ebce4d62b64be
Renato <renatossnt@gmail.com>
2023-02-04 11:36:50 -0300

    Adiciona tabela de funcionários - tabela de funcionários dinâmica - material icons no botão de editar documentos

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,7 +1,11 @@
 import { collection, getDocs, onSnapshot } from "firebase/firestore";
 import React from "react";
 import { db } from "../../firebaseConnection";
+import Button from "../Forms/Button";
+import styles from "./Dashboard.module.css";
 
+import IconButton from "@mui/material/IconButton";
+import EditIcon from "@mui/icons-material/Edit";
 const Dashboard = () => {
   const [employees, setEmployees] = React.useState([]);
 
@@ -14,6 +18,7 @@ const Dashboard = () => {
             id: doc.id,
             nome: doc.data().nome,
             idade: doc.data().idade,
+            cidade: doc.data().cidade,
           });
         });
         setEmployees(listOfEmployees);
@@ -23,17 +28,38 @@ const Dashboard = () => {
   }, []);
 
   return (
-    <div className="container">
-      <ul>
+    <table className="container">
+      <caption>Funcionários</caption>
+      <thead>
+        <tr>
+          {employees[0] &&
+            Object.keys(employees[0]).map((header, key) => (
+              <th key={key}>{header}</th>
+            ))}
+          <th>Detalhes</th>
+        </tr>
+      </thead>
+      <tbody>
         {employees.map((employee) => (
-          <li>
-            <span>{employee.id}</span>
-            <span>{employee.nome}</span>
-            <span>{employee.idade}</span>
-          </li>
+          <tr key={employee.id}>
+            <td>{employee.id}</td>
+            <td>{employee.nome}</td>
+            <td>{employee.idade}</td>
+            <td>{employee.cidade}</td>
+            <td>
+              <IconButton
+                color="primary"
+                aria-label="editar documentos"
+                // component="label"
+              >
+                {/* <input hidden accept="image/*" type="file" /> */}
+                <EditIcon />
+              </IconButton>
+            </td>
+          </tr>
         ))}
-      </ul>
-    </div>
+      </tbody>
+    </table>
   );
 };
 
